479e46b848e7c3a1b92f530029b52c6a90b53548
jojopipe <78083515+jojopipe@users.noreply.github.com>
2023-03-31 22:00:34 +0200

    a

--- cmake-build-debug/CMakeFiles/calculator.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -12,8 +12,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/joppo/Documents/calculator/main.c" "CMakeFiles/calculator.dir/main.c.o" "gcc" "CMakeFiles/calculator.dir/main.c.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- main.c
+++ main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 
+int sum(int a, int b);
+typedef struct{
+    double values[3];
+    int dimensions;
+
+} Vector3;
+double dotProduct(Vector3 v, Vector3 u);
+
 int main() {
     printf("Hello, World!\n");
-    printf("%d", sum(3, 4));
+    printf("%d\n", sum(3, 4));
+
+    Vector3 v = {.values = {3.0, 4.3, 2.6}};
+    Vector3 u = {.values = {2.0, 1.1, -3.2}};
+    printf("dotp: %f\n", dotProduct(v, u));
+
     return 0;
 }
 
 int sum(int a, int b){
     return (int) a + b;
+}
+
+double dotProduct(Vector3 v, Vector3 u){
+    double sum = 0.0;
+    for (int i = 0; i <= 3; i++){
+        sum += (v.values[i] * u.values[i]);
+    }
+    return sum;
 }
